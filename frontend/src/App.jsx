f989da1a4f71e7a655d80b9a7cd1636d594426f0
Simon Mekonnen Legesse <103356546+Leviathan913@users.noreply.github.com>
2025-01-29 15:19:57 +0300

    Files Added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
+/* import About from "./pages/About/About.jsx"; */
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+        {/* <Route path='/about' element={<About/>}/> */}
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
