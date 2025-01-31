b5f4264aaee962faf60f8f9ee4ea2fb7900d7a40
Simon Mekonnen Legesse <103356546+Leviathan913@users.noreply.github.com>
2025-01-31 06:58:31 +0300

    frontend done

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,54 +1,60 @@
-import React, { useState } from "react";
-import "../Navbar/Navbar.css";
+import React, { useContext, useState } from "react";
+import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount } = useContext(StoreContext);
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to={"/"}>
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
       <ul className="navbar-menu">
-        <li
+        <Link
+          to={"/"}
           onClick={() => setMenu("home")}
           className={menu === "home" ? "active" : ""}
         >
           HOME
-        </li>
-        <li
+        </Link>
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("menu")}
           className={menu === "menu" ? "active" : ""}
         >
           MENU
-        </li>
-        <li
+          {/* We should make this similar to collection page in shop cart and also add the searching functionalities */}
+        </a>
+        <a
+          href="#app-download"
           onClick={() => setMenu("mobile-app")}
           className={menu === "mobile-app" ? "active" : ""}
         >
           MOBILE-APP
-        </li>
+        </a>
 
         {/* Navbar for ABOUT */}
 
-        {/* <li
-          onClick={() => setMenu("about")}
-          className={menu === "about" ? "active" : ""}
-        >
-          ABOUT
-        </li> */}
-        <li
+        <a
+          href="#footer"
           onClick={() => setMenu("contact")}
           className={menu === "contact" ? "active" : ""}
         >
           CONTACT
-        </li>
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to={"/cart"}>
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
-        <button>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   );
