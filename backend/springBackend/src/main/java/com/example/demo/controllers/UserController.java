e2ff483f58ecba6eeb3e8858dfb90370325e9cdb
Helen <helutb@gmail.com>
2025-02-01 20:25:08 +0300

    springBackend, but still needs to be configured for postgre database

---
+++ backend/springBackend/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody User user) {
+        return userService.loginUser(user);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
